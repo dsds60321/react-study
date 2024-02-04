60712e159e00c619603b75ecfca5f31bdd5411f8
gh.kang <gh.kang@cyrexpay.com>
2024-02-04 20:52:45 +0900

    react-crash-course end

--- react-crash-course/src/main.jsx
+++ react-crash-course/src/main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
-import Posts from "./routes/Posts.jsx";
-import NewPost from "./routes/NewPost.jsx";
+import Posts, {loader as postsLoader} from "./routes/Posts.jsx";
+import NewPost, {action as newPostAction} from "./routes/NewPost.jsx";
+import PostDetails, {loader as postDetailsLoader} from "./components/PostDetail.jsx";
 import RootLayout from "./routes/ReactLayout.jsx";
 
 const router = createBrowserRouter([
@@ -14,7 +15,11 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Posts />,
-        children: [{ path: "/create-post", element: <NewPost /> }],
+        loader : postsLoader,
+        children: [
+            { path: "/create-post", element: <NewPost /> , action: newPostAction},
+            {path: "/:id", element: <PostDetails />, loader: postDetailsLoader }
+        ],
       },
       ,
     ],
--- react-crash-course/src/routes/NewPost.jsx
+++ react-crash-course/src/routes/NewPost.jsx
@@ -1,52 +1,46 @@
 import classes from "./NewPost.module.css";
-import { useState } from "react";
 import Modal from "../components/Modal.jsx";
-import { Link } from "react-router-dom";
+import {Link, Form, redirect} from "react-router-dom";
 
-function NewPost({ onAddPost }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  function submitHandler(event) {
-    event.preventDefault();
-    const postData = {
-      body: enteredBody,
-      author: enteredAuthor,
-    };
-    onAddPost(postData);
-    onCancel();
-  }
+function NewPost() {
   return (
     <Modal>
-      <form className={classes.form} onSubmit={submitHandler}>
+      <Form method="post" className={classes.form}>
         <p>
           <label htmlFor="body">Text</label>
-          <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
+          <textarea id="body" name="body" required rows={3} />
         </p>
         <p>
           <label htmlFor="name">Your name</label>
           <input
             type="text"
+            name="author"
             id="name"
             required
-            onChange={authorChangeHandler}
           />
         </p>
         <Link to={"/"} className={classes.actions}>
           <button type="button">Cancel</button>
           <button>Submit</button>
         </Link>
-      </form>
+      </Form>
     </Modal>
   );
 }
 
 export default NewPost;
+
+export async function action({request}) {
+  console.log(request)
+  const formData = await request.formData();
+  const postData = Object.fromEntries(formData);
+  await fetch('http://localhost:8080/posts', {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      'Content-Type' : 'application/json',
+    },
+  });
+
+  return redirect('/');
+}
